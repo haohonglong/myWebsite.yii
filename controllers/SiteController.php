8a41df15ac154a2f21fa5701e0625f4f01f8fbe8
龙昊宏 <947923503@qq.com>
2018-03-22 13:08:40 +0800

    提交说明

---
+++ controllers/SiteController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\db\Query;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+
+        $query = (new Query());
+        $rows = $query->select('s.id as pid,s.name as title,a.id AS id,a.name,a.url,a.info,a.date,a.sortsId')->from('video_addres as a')->leftJoin('sorts as s','s.id = a.sortsId')->all();
+
+        $list = [];
+        foreach ($rows as $key => $item){
+            if(!isset($list[$item['pid']])){
+                $list[$item['pid']] = [
+                    'pid'=>$item['pid'],
+                    'title'=>$item['title'],
+                    'child'=>[],
+                ];
+            }
+
+            $list[$item['pid']]['child'][] =[
+                'id'=>$item['id'],
+                'name'=>$item['name'],
+                'url'=>$item['url'],
+                'date'=>$item['date'],
+                'info'=>$item['info'],
+            ];
+
+
+        }
+        $list = array_values($list);
+        $var=[
+            'list'=>$list
+        ];
+
+        return $this->render('index',$var);
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
